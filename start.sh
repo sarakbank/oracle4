5fdac6898f7d8ac38c979285a7cf4c32fcd21910
sarakbank <115914735+sarakbank@users.noreply.github.com>
2022-10-16 07:55:45 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo, Please Wait..."
-git clone xxxxxx /oracle4
+git clone https://github.com/sarakbank/oracle4 /oracle4
 cd /oracle4
 echo "Starting Bot, Please Wait..."
 python3 bot.py
